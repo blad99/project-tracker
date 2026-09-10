43c52124f6bbef84055af02c83fca3a87d11a35e
xFardi <47803063+blad99@users.noreply.github.com>
2026-09-10 19:43:27 +0700

    first commit

---
+++ database/migrations/2026_09_10_123010_create_tasks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('project_id')->constrained('project')->cascadeOnDelete();
+            $table->unsignedBigInteger('parent_task_id')->nullable();
+            $table->string('name');
+            $table->enum('status', ['Draft', 'In Progress', 'Done'])->default('Draft');
+            $table->unsignedInteger('weight')->default(1);
+            $table->timestamps();
+            $table->foreign('parent_task_id')->references('id')->on('task')->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
---
+++ database/migrations/2026_09_10_123149_create_project_dependencies_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('project_dependencies', function (Blueprint $table) {
+            $table->unsignedBigInteger('project_id');
+            $table->unsignedBigInteger('depens_on_project_id');
+            $table->primary(['project_id', 'depends_on_project_id']);
+            $table->foreign('project_id')->references('id')->on('projects')->cascadeOnDelete();
+            $table->foreign('depends_on_project_id')->references('id')->on('projects')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('project_dependencies');
+    }
+};
