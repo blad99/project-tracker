11d1ef786da304d41f4c9d47816d0d5ed751cad4
xFardi <47803063+blad99@users.noreply.github.com>
2026-09-10 22:40:33 +0700

    feat: implement FormRequest validation Project and Task

---
+++ app/Http/Requests/storeProjectRequest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class storeProjectRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'start_date' => 'nullable|date',
+            'end_date' => 'nullable|date|after_or_equal:start_date',
+            'dependencies' => 'nullable|array',
+            'dependencies.*' => 'integer|exists:projects,id'
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Nama project tidak boleh kosong',
+            'end_date.after_or_equal' => 'End date tidak boleh sebelum start date.',
+            'dependencies.*.exists' => 'Project dependency tidak valid,'
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'message' => $validator->errors()->first(),
+            'errors'  => $validator->errors(),
+        ], 422));
+    }
+}
---
+++ app/Http/Requests/updateTaskRequest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+
+class updateTaskRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'project_id' => 'required|integer|exists:project,id',
+            'parent_task_id' => 'nullable|integer|exists:tasks,id',
+            'status' => 'required|in:Draft,In Progress,Done',
+            'weight' => 'required|integer|min:1|max:100',
+            'dependencies' => 'nullable|array',
+            'dependencies.*' => 'integer|exists:tasks,id'
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Nama task tidak boleh kosong.',
+            'project_id.exists' => 'Project tidak valid',
+            'status.in' => 'Status tidak valid',
+        ];
+    }
+
+    protected function failedValidation(Validator $validator) {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'message' => $validator->errors()->first(),
+            'errors'  => $validator->errors(),
+        ], 422));
+    }
+}
